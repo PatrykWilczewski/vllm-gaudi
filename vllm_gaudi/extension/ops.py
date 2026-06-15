26286edd3e7e9a22435601c65b6825372251379f
Patryk Wilczewski <patrykx.wilczewski@intel.com>
2026-06-15 13:08:46 +0300

    missing import

--- vllm_gaudi/extension/ops.py
+++ vllm_gaudi/extension/ops.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the Apache 2.0 license found in the
 # LICENSE file in the root directory of this source tree.
 ###############################################################################
-from typing import Callable, Optional, Tuple, List, Union
+from typing import Callable, Optional, Tuple, List, Union, Dict, Any
 import habana_frameworks.torch as htorch
 import torch
 import torch.nn.functional as F
